01b479a0ea75d1238ac7ba1eb60193d3218750c7
jhilikb <69856481+jhilikb@users.noreply.github.com>
2022-03-10 20:32:12 +0530

    Update and rename run.sh to launchenv.sh

--- docker/launchenv.sh
+++ docker/launchenv.sh
@@ -1,9 +1,3 @@
 apt update
 sudo docker run -it --rm --network=host --gpus all -v `pwd`:/home/NLBM pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime
-cd /home/NLBM
-mkdir build
-cd build
-mkdir bin
-cd bin
-cp /home/NLBM/models/* .
-cp /home/NLBM/python/* .
+
