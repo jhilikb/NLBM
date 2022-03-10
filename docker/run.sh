79b1706c357923bdd5074ec69ece71476478c7a6
jhilikb <69856481+jhilikb@users.noreply.github.com>
2022-03-10 20:26:49 +0530

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 apt update
-sudo docker run -it --rm --network=host --gpus all -v `pwd`/NLBM:/home/NLBM pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime
+sudo docker run -it --rm --network=host --gpus all -v `pwd`:/home/NLBM pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime
 cd /home/NLBM
 mkdir build
 cd build
